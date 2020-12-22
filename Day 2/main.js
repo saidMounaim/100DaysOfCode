0d611cd72221b5cbd0e74789074c56b05d70951d
Said Mounaim <saidmounaim00@gmail.com>
2020-12-22 21:41:50 +0100

    Finder Api App using Vanilla javaScript

--- Day 2/main.js
+++ Day 2/main.js
@@ -1,38 +1,80 @@
-const input = document.getElementById("search");
-const gridElement = document.querySelector(".grid-api");
+const input = document.getElementById('search');
+const gridElement = document.querySelector('.grid-api');
 
 function showItems(dataApis) {
-    gridElement.innerHTML = "";
-    dataApis.forEach(element => {
-        const itemElement = document.createElement("a");
-        itemElement.classList.add("item");
-        itemElement.setAttribute("href", `${element.Link}`);
-        itemElement.setAttribute("target", "_blank");
-        itemElement.innerHTML = `
+	gridElement.innerHTML = '';
+	dataApis.forEach((element) => {
+		const itemElement = document.createElement('a');
+		itemElement.classList.add('item');
+		itemElement.setAttribute('href', `${element.Link}`);
+		itemElement.setAttribute('target', '_blank');
+		itemElement.innerHTML = `
             <h2>${element.API}</h2>
             <span>${element.Category}</span>
             <p>${element.Description}</p>
-        `
-        gridElement.append(itemElement);
-
-    })
+        `;
+		gridElement.append(itemElement);
+	});
 }
 
 //fetch Api
 async function fetchApi() {
-    const data = await fetch(`https://api.publicapis.org/entries`).then(response => response.json());
-    showItems(data.entries);
+	const data = await fetch(`https://api.publicapis.org/entries`).then((response) => response.json());
+
+	const itemsLength = data.entries.length;
+	paginate(data.entries, itemsLength);
+}
+
+function paginate(items, itemsLength) {
+	let itemsPerPage = 8;
+	let currentPage = 1;
+
+	let totalPages = Math.ceil(itemsPerPage * itemsLength);
+
+	let pages = [];
+
+	for (let i = 1; i <= totalPages; i++) {
+		pages.push(i);
+	}
+
+	const next = document.querySelector('.next');
+	const prev = document.querySelector('.prev');
+
+	next.addEventListener('click', function () {
+		if (currentPage == totalPages) {
+			return false;
+		}
+		currentPage++;
+		filtredItems();
+	});
+
+	prev.addEventListener('click', function () {
+		if (currentPage == 1) {
+			return false;
+		}
+		currentPage--;
+		filtredItems();
+	});
+
+	function filtredItems() {
+		const indexOfLastItems = currentPage * itemsPerPage;
+		const indexOfFirstItems = indexOfLastItems - itemsPerPage;
+		const filtredItems = items.slice(indexOfFirstItems, indexOfLastItems);
+		showItems(filtredItems);
+	}
+	filtredItems();
 }
 
 async function searchApi(query) {
-    const data = await fetch(`https://api.publicapis.org/entries?category=${query}&https=true`).then(response => response.json());
-    showItems(data.entries);
+	const data = await fetch(`https://api.publicapis.org/entries?category=${query}&https=true`).then((response) =>
+		response.json()
+	);
+	paginate(data.entries, data.entries.length);
 }
 
 input.addEventListener('keyup', function (e) {
-    const value = e.target.value;
-    console.log(value);
-    searchApi(value);
-})
+	const value = e.target.value;
+	searchApi(value);
+});
 
 fetchApi();
