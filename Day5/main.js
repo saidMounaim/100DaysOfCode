c751e6d47ebc15da98f04345f03c8bd02ab3e2a8
saidMounaim <saidmounaim00@gmail.com>
2020-12-25 17:35:30 +0100

    Activity Card Component Using (HTML5, CSS3, JS & GSAP)

---
+++ Day5/main.js
@@ -0,0 +1,5 @@
+gsap.to(".card", 0.6, { opacity: 1, top: 0, delay: 0.3, ease: Power0.easeInOut });
+gsap.to(".card .leftSide", 0.6, { opacity: 1, left: 0, delay: 1, ease: Power0.easeInOut });
+gsap.to(".header h3", 0.5, { opacity: 1, top: 0, delay: 0.7, ease: Power0.easeInOut });
+gsap.to(".header p", 0.5, { opacity: 1, top: 0, delay: 0.7, ease: Power0.easeInOut });
+gsap.to(".item", 0.5, { opacity: 1, top: 0, stagger: 0.5, delay: 0.7, ease: Power0.easeInOut });
