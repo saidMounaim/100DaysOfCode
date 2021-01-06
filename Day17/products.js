5df8f9818f33dc784d46e4892254c610b99563c5
saidMounaim <saidmounaim00@gmail.com>
2021-01-06 17:36:37 +0100

    Day 17 Starter Files

---
+++ Day17/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        name: 'Airpods Wireless Bluetooth Headphones',
+        image: '/images/airpods.jpg',
+        price: 89.99,
+    },
+    {
+        name: 'iPhone 11 Pro 256GB Memory',
+        image: '/images/phone.jpg',
+        price: 599.99,
+    },
+    {
+        name: 'Cannon EOS 80D DSLR Camera',
+        image: '/images/camera.jpg',
+        price: 929.99,
+    },
+    {
+        name: 'Sony Playstation 4 Pro White Version',
+        image: '/images/playstation.jpg',
+        price: 399.99,
+    },
+    {
+        name: 'Logitech G-Series Gaming Mouse',
+        image: '/images/mouse.jpg',
+        price: 49.99,
+    },
+    {
+        name: 'Amazon Echo Dot 3rd Generation',
+        image: '/images/alexa.jpg',
+        price: 29.99,
+    },
+]
