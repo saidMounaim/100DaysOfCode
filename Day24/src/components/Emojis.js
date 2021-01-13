535ed219a868b28bd1bb7f551d742e60dd4903e6
Said Mounaim <saidmounaim00@gmail.com>
2021-01-13 20:39:59 +0100

    Emoji Finder Using React Hooks

--- Day24/src/components/Emojis.js
+++ Day24/src/components/Emojis.js
@@ -1,41 +1,45 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Loader from './Loader';
 
 const Emojis = () => {
-    const [allEmojis, setAllEmojis] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [query, setQuery] = useState("");
+	const [allEmojis, setAllEmojis] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [query, setQuery] = useState('');
 
-    const fetchEmojis = async () => {
-        const res = await fetch(`https://emoji-api.com/emojis?search=${query}&access_key=52ab583bcd1b175d5aab0b259c0ecbc81d42abcc`);
-        const data = await res.json();
-        setAllEmojis(data);
-        setLoading(false);
-    }
+	const fetchEmojis = async () => {
+		const res = await fetch(
+			`https://emoji-api.com/emojis?search=${query}&access_key=52ab583bcd1b175d5aab0b259c0ecbc81d42abcc`
+		);
+		const data = await res.json();
+		setAllEmojis(data);
+		setLoading(false);
+	};
 
-    useEffect(() => {
-        fetchEmojis();
-    }, [query])
+	useEffect(() => {
+		fetchEmojis();
+	}, [query]);
 
-    return (
-        <div className="emojis" >
+	return (
+		<div className="emojis">
+			<div className="search">
+				<input placeholder="Search for keyword" value={query} onChange={(e) => setQuery(e.target.value)} />
+			</div>
 
-            <div className="search">
-                <input placeholder="Search for keyword" value={query} onChange={(e) => setQuery(e.target.value)} />
-            </div>
+			<div className="grid-emojis">
+				{loading === true ? (
+					<Loader />
+				) : (
+					allEmojis?.slice(0, 9).map((emoji) => (
+						<div className="item" key={emoji.title}>
+							<span>{emoji.character}</span>
+						</div>
+					))
+				)}
 
-            <div className="grid-emojis">
+				{allEmojis === null && <h1>No Emoji Found</h1>}
+			</div>
+		</div>
+	);
+};
 
-                {loading === true ? <Loader /> : allEmojis.slice(0, 9).map(emoji =>
-                    <div className="item" key={emoji.title}>
-                        <span>{emoji.character}</span>
-                    </div>
-                )}
-
-            </div>
-
-        </div>
-    )
-}
-
-export default Emojis
+export default Emojis;
